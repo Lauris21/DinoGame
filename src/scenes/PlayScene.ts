e8deee8ac767bcabc132ce5efc5fcd8f31935470
Lauris21 <laurasanchez0301@gmail.com>
2023-09-19 12:32:45 +0200

    Preload Scene & Play Scene

---
+++ src/scenes/PlayScene.ts
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+class PlayScene extends Phaser.Scene {
+    constructor() {
+        super("PlayScene")
+    }
+
+    create() {
+        
+    }
+}
+
+export default PlayScene
